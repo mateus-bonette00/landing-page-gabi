8e3b106d1ecc7b8f4749afcb0cd3cb61dda90057
mateus.bonette00 <mateus.bonette00@gmail.com>
2026-02-22 01:36:11 -0300

    att3

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body>
         <Header />
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
