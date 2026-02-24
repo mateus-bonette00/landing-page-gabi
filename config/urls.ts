4c3bbb2538a32c46640d5c2c7c34c4df83f6d364
mateus.bonette00 <mateus.bonette00@gmail.com>
2026-02-24 19:21:12 -0300

    att

--- config/urls.ts
+++ config/urls.ts
@@ -6,4 +6,5 @@ export const SALES_PAGE_URL = "https://kiwify.app/RK4COzl";
 export const CHECKOUT_PLUS_URL = "https://pay.kiwify.com.br/IdLuGU4";
 export const CHECKOUT_BASICO_URL = "https://pay.kiwify.com.br/nDYqutU";
 export const COURSE_PLATFORM_URL = "https://kiwify.com.br/area-de-membros";
-export const WHATSAPP_URL = "https://wa.me/5535999940656";
+export const WHATSAPP_URL =
+  "https://wa.me/5535999940656?text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20desafio%20pilates%208GX";
